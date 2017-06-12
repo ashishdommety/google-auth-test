0c5095edf3981401c0533c3c4558976e70166db9
ashishdommety <ashishuiux@gmail.com>
2017-06-12 15:55:21 -0700

    login functionality?

--- app.js
+++ app.js
@@ -12,17 +12,22 @@ $(document).ready(function() {
 
   //instance of google provider
   var provider = new firebase.auth.GoogleAuthProvider();
-
+  var user, photo;
+  $('#content').hide();
   $('#sign-in').on('click', function() {
     firebase.auth().signInWithPopup(provider).then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;
       // The signed-in user info.
-      var user = result.user;
+      user = result.user;
       console.log(user.displayName);
       console.log(user.email);
       console.log(user.photoURL);
       console.log(user.uid);
+      $('#content').show();
+      $('#login').hide();
+      $('#welcomeText').text('Welcome, '+ user.displayName);
+      $('#pic').attr("src",user.photoURL);
       // ...
     }).catch(function(error) {
       // Handle Errors here.
